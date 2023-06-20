49894450ae5456ba45babbdd2a1a7cb7ed4c51b3
Tony Minjae Lee <tony@treasurer.co.kr>
2023-06-20 17:11:26 +0900

    commonJS

--- var.js
+++ var.js
@@ -1,9 +1,15 @@
-const odd = "cjs odd";
-const even = "cjs even";
+// const odd = "cjs odd";
+// const even = "cjs even";
 
 // module.exports : 변수들을 담은 객체
 // 파일이 모듈로 기능하게 됨
-module.exports = {
-  odd,
-  even,
-};
+// module.exports = {
+//   odd,
+//   even,
+// };
+
+// 각 변수를 exports 객체에 하나씩 할당
+console.log(module.exports === exports);
+
+exports.odd = "cjs 짝";
+exports.even = "cjs 홀";
