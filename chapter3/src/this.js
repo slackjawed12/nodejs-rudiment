7384fcb44c1fa4c861d952395a2b47ea8ae20928
narustudio <slackjawed12@gmail.com>
2023-09-17 21:11:26 +0900

    feature: node.js에서 this가 가리키는 것은 module.exports이다

---
+++ chapter3/src/this.js
@@ -0,0 +1,13 @@
+const assert = require("assert");
+// node에서 this는 module.exports(exports 객체, {})를 가리킨다.
+console.log(this);
+console.log(assert(this === module.exports));
+console.log(assert(this === exports));
+
+// 함수 내부의 his는 global 객체를 가리킨다.
+function thisInFunction() {
+  console.log("function in this :", this === global);
+  console.log(global);
+}
+
+thisInFunction();
