bb9d98e407603983549c912628d3646559247b53
narustudio <slackjawed12@gmail.com>
2023-09-21 22:26:29 +0900

    feature: process 객체의 다양한 속성 확인

---
+++ chapter3/src/process/process.js
@@ -0,0 +1,8 @@
+console.log("process.version : ", process.version);
+console.log("process.arch : ", process.arch);
+console.log("process.platform : ", process.platform);
+console.log("process.pid : ", process.pid);
+console.log("process.uptime() : ", process.uptime());
+console.log("process.execPath : ", process.execPath); // 노드 경로가 나옴
+console.log("process.cwd() : ", process.cwd());
+console.log("process.cpuUsage() : ", process.cpuUsage());
--- chapter3/src/var.js
+++ chapter3/src/var.js
@@ -1,7 +1,6 @@
 const odd = "CJS 홀수입니다";
 const even = "CJS 짝수입니다";
 // exports 변수와 module.exports 참조가 달라진다.
-// require는 module.exports를 리턴하므로 etc는 사용하지 못함
 exports.etc = "기타 변수";
 module.exports = {
   odd,
