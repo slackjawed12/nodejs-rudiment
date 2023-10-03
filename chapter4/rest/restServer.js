a5122c85deaf00ac4dd2df754ecd74c39e07e000
narustudio <slackjawed12@gmail.com>
2023-10-03 21:32:08 +0900

    chapter4: rest api 구현 - get users, post, put, delete

--- chapter4/rest/restServer.js
+++ chapter4/rest/restServer.js
@@ -2,6 +2,9 @@ const http = require("http");
 const fs = require("fs").promises;
 const path = require("path");
 
+// 임시 유저 데이터 저장용 객체
+const users = {};
+
 http
   .createServer(async (req, res) => {
     try {
@@ -17,12 +20,52 @@ http
           const data = await fs.readFile(path.join(__dirname, "about.html"));
           res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
           return res.end(data);
+        } else if (req.url === "/users") {
+          res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+          return res.end(JSON.stringify(users));
         }
         try {
           const data = await fs.readFile(path.join(__dirname, req.url));
           return res.end(data);
         } catch (err) {
           // 404 error
+        }
+      } else if (req.method === "POST") {
+        if (req.url === "/user") {
+          let body = "";
+          req.on("data", (data) => {
+            body += data;
+          });
+          return req.on("end", () => {
+            console.log("POST 본문(Body): ", body);
+            const { name } = JSON.parse(body);
+            const id = Date.now();
+            users[id] = name;
+            res.writeHead(201, { "Content-Type": "text/html; charset=utf-8" });
+            res.end("등록 성공");
+          });
+        }
+      } else if (req.method === "PUT") {
+        if (req.url.startsWith("/user")) {
+          const key = req.url.split("/")[2];
+          let body = "";
+          req.on("data", (data) => {
+            body += data;
+          });
+          return req.on("end", () => {
+            console.log("PUT 본문(Body): ", body);
+            users[key] = JSON.parse(body).name;
+            res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+            return res.end(JSON.stringify(users));
+          });
+        }
+      } else if (req.method === "DELETE") {
+        if (req.url.startsWith("/user")) {
+          const key = req.url.split("/")[2];
+          console.log(users[key]);
+          delete users[key];
+          res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+          return res.end(JSON.stringify(users));
         }
       }
       res.writeHead(404);
