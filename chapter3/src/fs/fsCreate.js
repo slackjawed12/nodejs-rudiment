9121a299e4e12495de846737f56e8bd6e2e60925
narustudio <slackjawed12@gmail.com>
2023-10-03 17:00:42 +0900

    chapter3: fs 모듈 - access, mkdir, rename, readdir, unlink, rmdir

---
+++ chapter3/src/fs/fsCreate.js
@@ -0,0 +1,34 @@
+const fs = require("fs").promises;
+const constants = require("fs").constants;
+
+fs.access(
+  "chapter3/src/fs/folder",
+  constants.F_OK | constants.W_OK | constants.R_OK
+)
+  .then(() => {
+    return Promise.reject("이미 폴더 있음");
+  })
+  .catch((err) => {
+    if (err.code === "ENOENT") {
+      console.log("폴더 없음");
+      return fs.mkdir("chapter3/src/fs/folder");
+    }
+    return Promise.reject(err);
+  })
+  .then(() => {
+    console.log("폴더 만들기 성공");
+    return fs.open("chapter3/src/fs/folder/file.js", "w");
+  })
+  .then((fd) => {
+    console.log("빈 파일 만들기 성공", fd);
+    return fs.rename(
+      "chapter3/src/fs/folder/file.js",
+      "chapter3/src/fs/folder/newfile.js"
+    );
+  })
+  .then(() => {
+    console.log("이름 바꾸기 성공");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
