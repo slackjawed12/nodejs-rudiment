23a6c544d3c9d188bf768a4fa55bf41d9e70096a
narustudio <slackjawed12@gmail.com>
2023-10-10 22:16:02 +0900

    chapter6: res.locals은 요청 한 개 scope를 갖는 공유데이터를 저장할 때 활용

--- chapter6/app.js
+++ chapter6/app.js
@@ -63,6 +63,17 @@ app.use((err, req, res, next) => {
   res.status(500).send(err.message);
 });
 
+app.use(
+  "/locals",
+  (req, res, next) => {
+    res.locals.data = "삽입된 로컬데이터";
+    next();
+  },
+  (req, res, next) => {
+    res.json(res.locals.data);
+  }
+);
+
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 포트에서 대기 중");
 });
