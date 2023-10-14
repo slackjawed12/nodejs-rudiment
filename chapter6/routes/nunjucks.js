6ecaa4f113bd8fa59067f727a0bfcbd45a281c80
narustudio <slackjawed12@gmail.com>
2023-10-14 21:58:01 +0900

    chapter6: nunjucks 문법 - 반복문, 조건문

--- chapter6/routes/nunjucks.js
+++ chapter6/routes/nunjucks.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.render("index", { title: "express" });
+  res.render("index_test", { title: "express" });
 });
 
 export default router;
