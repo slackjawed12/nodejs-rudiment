c6204d8baa059a0da46b000ea3eaae139e38cd32
narustudio <slackjawed12@gmail.com>
2023-10-15 19:01:21 +0900

    chapter6: nunjucks - layout, extends

--- chapter6/routes/nunjucks.js
+++ chapter6/routes/nunjucks.js
@@ -6,4 +6,12 @@ router.get("/", (req, res, next) => {
   res.render("index_test", { title: "express" });
 });
 
+router.get("/include", (req, res, next) => {
+  res.render("main", { title: "express" });
+});
+
+router.get("/extends", (req, res, next) => {
+  res.render("body", { title: "express" });
+});
+
 export default router;
