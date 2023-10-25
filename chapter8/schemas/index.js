d6a8077315de10b81e0e0789106455eb7c94feb7
narustudio <slackjawed12@gmail.com>
2023-10-25 23:48:03 +0900

    chapter8: mongoose object init

---
+++ chapter8/schemas/index.js
@@ -0,0 +1,18 @@
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const connect = await mongoose
+  .connect(`mongodb://root:${process.env.DB_PASSWORD}@localhost:27017/admin`, {
+    dbName: "nodejs",
+    useNewUrlParser: true,
+  })
+  .then((obj) => {
+    console.log(obj);
+    console.log("몽고디비 연결 성공");
+  })
+  .catch((error) => {
+    console.log("몽고디비 연결 실패");
+  });
+
+export default connect;
