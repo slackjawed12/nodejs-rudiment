c576e16c536e33557aaeb2c5494ccb10d05a99cd
narustudio <slackjawed12@gmail.com>
2023-11-05 00:47:47 +0900

    chapter9: 페이지 라우팅

--- chapter9/routes/page.js
+++ chapter9/routes/page.js
@@ -1,19 +1,25 @@
 import express from "express";
 import { isLoggedIn, isNotLoggedIn } from "../middlewares/index.js";
-import { renderProfile, renderJoin, renderMain } from "../controllers/page.js";
+import {
+  renderProfile,
+  renderJoin,
+  renderMain,
+  renderHashtag,
+} from "../controllers/page.js";
 
 const router = express.Router();
 
 router.use((req, res, next) => {
   res.locals.user = req.user;
-  res.locals.followerCount = 0;
-  res.locals.followingCount = 0;
-  res.locals.followingIdList = [];
+  res.locals.followerCount = req.user?.Followers?.length || 0;
+  res.locals.followingCount = req.user?.Followings?.length || 0;
+  res.locals.followingIdList = req.user?.Followings?.map((f) => f.id) || [];
   next();
 });
 
 router.get("/", renderMain);
 router.get("/profile", isLoggedIn, renderProfile);
 router.get("/join", isNotLoggedIn, renderJoin);
+router.get("/hashtag", renderHashtag);
 
 export default router;
