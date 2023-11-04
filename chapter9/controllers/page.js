88716920e03392177377418901e72e86ded7fea3
narustudio <slackjawed12@gmail.com>
2023-11-05 00:25:12 +0900

    chapter9: page renderer fix

--- chapter9/controllers/page.js
+++ chapter9/controllers/page.js
@@ -1,3 +1,6 @@
+import db from "../models/index.js";
+const { Post, User } = db;
+
 const renderProfile = (req, res) => {
   res.render("profile", { title: "내 정보 - NodeBird" });
 };
@@ -6,12 +9,23 @@ const renderJoin = (req, res) => {
   res.render("join", { title: "회원 가입 - NodeBird" });
 };
 
-const renderMain = (req, res, next) => {
-  const twits = [];
-  res.render("main", {
-    title: "NodeBird",
-    twits,
-  });
+const renderMain = async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ["id", "nick"],
+      },
+      order: [["createdAt", "DESC"]],
+    });
+    res.render("main", {
+      title: "NodeBird",
+      twits: posts,
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 };
 
 export { renderProfile, renderJoin, renderMain };
