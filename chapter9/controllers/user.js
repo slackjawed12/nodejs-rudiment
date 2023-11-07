f72d7c783ca48a81e7d1fd7fdd46d1e5b5e49fd1
narustudio <slackjawed12@gmail.com>
2023-11-07 23:42:30 +0900

    chapter9: 팔로잉 취소 컨트롤러 로직 작성

--- chapter9/controllers/user.js
+++ chapter9/controllers/user.js
@@ -19,3 +19,30 @@ export const follow = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteFollow = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: {
+        model: User,
+        as: "Followers",
+        where: {
+          id: req.user.id,
+        },
+      },
+    });
+    if (user) {
+      const follow = user.Followers.find((v) => v.id === req.user.id);
+      await follow.Follow.destroy();
+      res.send("success");
+    } else {
+      res.status(404).send("no user");
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
