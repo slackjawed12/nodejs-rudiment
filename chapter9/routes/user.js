2cf26ad11ad721e394754c75df7cf014d706ebe1
narustudio <slackjawed12@gmail.com>
2023-11-07 23:41:42 +0900

    chapter9: 팔로잉 취소 라우터 추가

--- chapter9/routes/user.js
+++ chapter9/routes/user.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { isLoggedIn } from "../middlewares/index.js";
-import { follow } from "../controllers/user.js";
+import { follow, deleteFollow } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.post("/:id/follow", isLoggedIn, follow);
-
+router.delete("/:id/follow", isLoggedIn, deleteFollow);
 export default router;
