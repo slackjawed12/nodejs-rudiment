c88be5be37237c266dce14f78366ed986bad6586
narustudio <slackjawed12@gmail.com>
2023-11-14 23:35:16 +0900

    chapter10-1: nodebird client 프로젝트 main

---
+++ chapter10-1/app.js
@@ -0,0 +1,48 @@
+import express from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import nunjucks from "nunjucks";
+import dotenv from "dotenv";
+import indexRouter from "./routes/index.js";
+dotenv.config();
+
+const app = express();
+app.set("port", process.env.PORT || 4000);
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
+
+app.use(morgan("dev"));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+
+app.use("/", indexRouter);
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기 중");
+});
