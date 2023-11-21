292262484f20c1024136c7cb64084fcbd96ef6b0
narustudio <slackjawed12@gmail.com>
2023-11-21 23:53:38 +0900

    chapter11: isNotLoggedIn 테스트

--- chapter10/middlewares/index.spec.js
+++ chapter10/middlewares/index.spec.js
@@ -4,6 +4,7 @@ import { jest } from "@jest/globals";
 
 describe("isLoggedIn", () => {
   const res = {
+    // status는 chaining 해야하므로 res 반환으로 mock
     status: jest.fn(() => res),
     send: jest.fn(),
   };
@@ -13,6 +14,7 @@ describe("isLoggedIn", () => {
       isAuthenticated: jest.fn(() => true),
     };
     isLoggedIn(req, res, next);
+    // toBeCalledTimes : 몇번 호출되었는지
     expect(next).toBeCalledTimes(1);
   });
   test("로그인되어 있지 않으면 에러를 응답한다.", () => {
@@ -20,11 +22,29 @@ describe("isLoggedIn", () => {
       isAuthenticated: jest.fn(() => false),
     };
     isLoggedIn(req, res, next);
+    // toBeCalledWith : 특정 인수와 함께 호출되었는지
     expect(res.status).toBeCalledWith(403);
     expect(res.send).toBeCalledWith("로그인 필요");
   });
 });
 describe("isNotLoggedIn", () => {
-  test("로그인되어 있으면 에러를 응답한다.", () => {});
-  test("로그인되어 있지 않으면 next를 호출한다.", () => {});
+  const res = {
+    redirect: jest.fn(),
+  };
+  const next = jest.fn();
+  test("로그인되어 있으면 에러를 응답한다.", () => {
+    const req = {
+      isAuthenticated: jest.fn(() => true),
+    };
+    isNotLoggedIn(req, res, next);
+    const message = encodeURIComponent("로그인한 상태입니다.");
+    expect(res.redirect).toBeCalledWith(`/?error=${message}`);
+  });
+  test("로그인되어 있지 않으면 next를 호출한다.", () => {
+    const req = {
+      isAuthenticated: jest.fn(() => false),
+    };
+    isNotLoggedIn(req, res, next);
+    expect(next).toHaveBeenCalledTimes(1);
+  });
 });
