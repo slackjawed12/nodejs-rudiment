a64de2eb4a08ef70b62426d50402e5db201bf59a
narustudio <slackjawed12@gmail.com>
2023-11-26 19:48:10 +0900

    chapter12 웹소켓 객체 생성을 socket.io 형식으로 변경 - 서버

--- chapter12/socket.js
+++ chapter12/socket.js
@@ -1,31 +1,31 @@
-const WebSocket = require("ws");
+// const WebSocket = require("ws");
+const SocketIO = require("socket.io");
 
 module.exports = (server) => {
   // 인자로 들어온 server 객체(express 서버)를 웹소켓 서버와 연결한다.
-  const wss = new WebSocket.Server({ server });
-  wss.on("connection", (ws, req) => {
+  const io = new SocketIO(server, { path: "/socket.io" });
+
+  io.on("connection", (socket) => {
     // 웹 소켓 연결 시 - 클라이언트 ip 확인
+    const req = socket.request;
     const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-    console.log("새로운 클라이언트 접속", ip);
-    ws.on("message", (message) => {
-      // 클라이언트로부터 메시지 수신 시
-      console.log(message.toString());
-    });
-    ws.on("error", (error) => {
-      // 에러 시
-      console.error(error);
-    });
-    ws.on("close", () => {
+    console.log("새로운 클라이언트 접속", ip, socket.id, req.ip);
+    socket.on("disconnect", () => {
       // 연결 종료 시
       console.log("클라이언트 접속 해제", ip);
       clearInterval(ws.interval);
     });
-
+    socket.on("reply", (data) => {
+      // 클라이언트로부터 메시지 수신 시
+      console.log(data);
+    });
+    socket.on("error", (error) => {
+      // 에러 시
+      console.error(error);
+    });
     // 3초마다 클라이언트로 메시지 전송
-    ws.interval = setInterval(() => {
-      if (ws.readyState === ws.OPEN) {
-        ws.send("서버에서 클라이언트로 메시지를 보냅니다.");
-      }
+    socket.interval = setInterval(() => {
+      socket.emit("news", "Hello Socket.IO");
     }, 3000);
   });
 };
