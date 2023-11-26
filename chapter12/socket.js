775ff3d4cdc2a76e1789469be01fe01634d36b94
narustudio <slackjawed12@gmail.com>
2023-11-26 22:15:21 +0900

    chapter12: socket.io 네임스페이스 수정

--- chapter12/socket.js
+++ chapter12/socket.js
@@ -1,32 +1,29 @@
 // const WebSocket = require("ws");
 const SocketIO = require("socket.io");
 
-module.exports = (server) => {
+module.exports = (server, app) => {
   // 인자로 들어온 server 객체(express 서버)를 웹소켓 서버와 연결한다.
   const io = SocketIO(server, { path: "/socket.io" });
+  app.set("io", io);
+  const room = io.of("/room");
+  const chat = io.of("/chat");
 
-  io.on("connection", (socket) => {
-    // 웹 소켓 연결 시 - 클라이언트 ip 확인
-    const req = socket.request;
-    const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-    console.log("새로운 클라이언트 접속", ip, socket.id, req.ip);
+  room.on("connection", (socket) => {
+    console.log("room 네임스페이스에 접속");
     socket.on("disconnect", () => {
-      // 연결 종료 시
-      console.log("클라이언트 접속 해제", ip);
-      clearInterval(ws.interval);
+      console.log("room 네임스페이스 접속 해제");
     });
-    /** reply : 사용자가 직접 설정한 이벤트 이름 - ws 모듈과 차이점 */
-    socket.on("reply", (data) => {
-      // 클라이언트로부터 메시지 수신 시
-      console.log(data);
+  });
+
+  chat.on("connection", (socket) => {
+    console.log("chat 네임스페이스에 접속");
+
+    socket.on("join", (data) => {
+      socket.join(data);
     });
-    socket.on("error", (error) => {
-      // 에러 시
-      console.error(error);
+
+    socket.on("disconnect", () => {
+      console.log("chat 네임스페이스 접속 해제");
     });
-    // 3초마다 클라이언트로 메시지 전송
-    socket.interval = setInterval(() => {
-      socket.emit("news", "Hello Socket.IO");
-    }, 3000);
   });
 };
