1f2bf62a2809af8249041a5c447a5bcdd873a374
narustudio <slackjawed12@gmail.com>
2023-11-26 22:16:00 +0900

    chapter12: controller, route 작성 - chat, room

--- chapter12/routes/index.js
+++ chapter12/routes/index.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const {
+  renderRoom,
+  renderMain,
+  enterRoom,
+  removeRoom,
+  createRoom,
+} = require("../controllers");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("index");
-});
+router.get("/", renderMain);
+
+router.get("/room", renderRoom);
+
+router.post("/room", createRoom);
+
+router.get("/room/:id", enterRoom);
+
+router.delete("/room/:id", removeRoom);
 
 module.exports = router;
