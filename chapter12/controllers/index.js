ee2838aa18faf853fbec73f2e6e5baf68d8f645b
narustudio <slackjawed12@gmail.com>
2023-11-27 22:52:29 +0900

    chapter12: controller 주석 추가

--- chapter12/controllers/index.js
+++ chapter12/controllers/index.js
@@ -24,6 +24,7 @@ exports.createRoom = async (req, res, next) => {
       password: req.body.password,
     });
     const io = req.app.get("io");
+    // /room 네임스페이스에 연결한 모든 클라이언트에 데이터를 보낸다.
     io.of("/room").emit("newRoom", newRoom);
     if (req.body.password) {
       res.redirect(`/room/${newRoom._id}?password=${req.body.password}`);
@@ -36,6 +37,7 @@ exports.createRoom = async (req, res, next) => {
   }
 };
 
+// 채팅방에 접속해서 채팅방 화면을 렌더링
 exports.enterRoom = async (req, res, next) => {
   try {
     const room = await Room.findOne({ _id: req.params.id });
@@ -46,7 +48,9 @@ exports.enterRoom = async (req, res, next) => {
       return res.redirect("/?error=비밀번호가 틀렸습니다.");
     }
     const io = req.app.get("io");
+    // adapter.rooms = chat 네임스페이스의 room들 - 각각이 채팅방임
     const { rooms } = io.of("/chat").adapter;
+    // rooms.get(id) : 해당 방 아이디(id)의 소켓 목록 - size가 소켓 수이므로 참가 인원이 됨
     if (room.max <= rooms.get(req.params.id)?.size) {
       return res.redirect("/?error=허용 인원을 초과했습니다.");
     }
@@ -62,8 +66,10 @@ exports.enterRoom = async (req, res, next) => {
   }
 };
 
+// 채팅방을 삭제
 exports.removeRoom = async (req, res, next) => {
   try {
+    // 채팅방과 채팅 내역 삭제
     await Room.remove({ _id: req.params.id });
     await Chat.remove({ room: req.params.id });
     res.send("ok");
