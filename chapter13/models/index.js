13690e2cb942f9e07b64f01ab25f8f561f2582ed
narustudio <slackjawed12@gmail.com>
2023-12-04 19:30:30 +0900

    chapter13 - config import 수정

--- chapter13/models/index.js
+++ chapter13/models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require("sequelize");
 const process = require("process");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require("../config")[env];
+const config = require("../config/config")[env];
 const db = {};
 
 const sequelize = new Sequelize(
@@ -26,10 +26,7 @@ fs.readdirSync(__dirname)
     );
   })
   .forEach((file) => {
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes
-    );
+    const model = require(path.join(__dirname, file));
     db[model.name] = model;
     model.initiate(sequelize);
   });
