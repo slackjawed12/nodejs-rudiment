614821bd3c5a3c5e039e3a77bcf1ec5b82b66327
narustudio <slackjawed12@gmail.com>
2023-12-06 23:48:55 +0900

    chapter14 - 입력값 틀릴 경우 종료하지 않고 다시 입력

--- chapter14/index.js
+++ chapter14/index.js
@@ -1,20 +1,38 @@
 #!/usr/bin/env node
 const readline = require("readline");
 
+// rl 객체 생성
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+  input: process.stdin, // 콘솔 입력 스트림
+  output: process.stdout, // 콘솔 출력 스트림
 });
 
-rl.question("예제가 재미있습니까? (y/n) ", (answer) => {
+// 두 번째 인수 콜백 : 사용자 입력 인자(answer)로 받고 출력
+// rl.question("예제가 재미있습니까? (y/n) ", (answer) => {
+//   if (answer === "y") {
+//     console.log("감사합니다!");
+//   } else if (answer === "n") {
+//     console.log("죄송합니다.");
+//   } else {
+//     console.log("y 또는 n만 입력하세요.");
+//   }
+//   rl.close();
+// });
+
+// y, n 입력하지 않았을 경우 종료되지 않고 다시 입력
+console.clear();
+const answerCallback = (answer) => {
   if (answer === "y") {
     console.log("감사합니다!");
+    rl.close();
   } else if (answer === "n") {
     console.log("죄송합니다.");
+    rl.close();
   } else {
+    console.clear();
     console.log("y 또는 n만 입력하세요.");
+    rl.question("예제는 재미있습니까? (y/n) ", answerCallback);
   }
-  rl.close();
-});
+};
 
-// console.log("Hello CLI", process.argv);
+rl.question("예제가 재미있습니까? (y/n) ", answerCallback);
