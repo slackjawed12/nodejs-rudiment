d0dfc6e938280007d3029a6e8b301367060f2a12
narustudio <slackjawed12@gmail.com>
2023-12-06 23:42:20 +0900

    chapter14 - readline 모듈 사용해서 사용자 입력 받기

--- chapter14/index.js
+++ chapter14/index.js
@@ -1,2 +1,20 @@
 #!/usr/bin/env node
-console.log("Hello CLI");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("예제가 재미있습니까? (y/n) ", (answer) => {
+  if (answer === "y") {
+    console.log("감사합니다!");
+  } else if (answer === "n") {
+    console.log("죄송합니다.");
+  } else {
+    console.log("y 또는 n만 입력하세요.");
+  }
+  rl.close();
+});
+
+// console.log("Hello CLI", process.argv);
