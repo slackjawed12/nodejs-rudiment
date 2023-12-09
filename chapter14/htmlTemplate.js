a1a0c993ff1894c4f73167981ae5898218d8a958
narustudio <slackjawed12@gmail.com>
2023-12-09 18:37:19 +0900

    chapter14 - cli 템플릿 프로그램 실습 직접 작성 버전

--- chapter14/htmlTemplate.js
+++ chapter14/htmlTemplate.js
@@ -8,8 +8,6 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-const [fileExt, fileName, path] = process.argv.slice(2);
-console.log(fileExt, fileName, path);
 const htmlTemplate = `<!DOCTYPE html>
 <html>
   <head>
@@ -21,3 +19,62 @@ const htmlTemplate = `<!DOCTYPE html>
   </body>
 </html>
 `;
+
+const routerTemplate = `
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  try {
+    res.send(<ok>);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+})
+
+module.exports = router;
+`;
+
+const executeTemplateCommand = () => {
+  const [fileExt, fileName, filePath] = process.argv.slice(2);
+  checkPathAndCreateDirectory(filePath);
+  makeTemplateFile(fileExt, filePath, fileName);
+};
+
+const checkPathAndCreateDirectory = (filePath) => {
+  try {
+    const x = fs.readdirSync(filePath);
+  } catch (err) {
+    fs.mkdirSync(filePath);
+  }
+};
+
+const makeTemplateFile = (fileExt, filePath, fileName) => {
+  try {
+    const readResult = fs.readFileSync(`${filePath}/${fileName}.${fileExt}`);
+    if (readResult) {
+      throw new Error("해당 경로에 이미 파일이 있습니다.");
+    }
+
+    if (fileExt === "html") {
+      makeHtmlTemplate(fileExt, filePath, fileName);
+      return;
+    }
+    if (fileExt === "ts" || fileExt === "js") {
+      makeRouterTemplate(fileExt, filePath, fileName);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const makeHtmlTemplate = (fileExt, filePath, fileName) => {
+  fs.writeFileSync(`${filePath}/${fileName}.${fileExt}`, htmlTemplate);
+};
+
+const makeRouterTemplate = (fileExt, filePath, fileName) => {
+  fs.writeFileSync(`${filePath}/${fileName}.${fileExt}`, routerTemplate);
+};
+
+executeTemplateCommand();
